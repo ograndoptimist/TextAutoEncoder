fec73dc7ffeeb4ed3b624da10ad8fbd59556a16e
gabriel.pontes <gabrieliprj@gmail.com>
2019-10-10 12:14:26 -0300

    params from constructor updated and now fully fixed

--- source/model/autoencoder.py
+++ source/model/autoencoder.py
@@ -12,13 +12,14 @@ class AutoEncoder(nn.Module):
     def __init__(self,
                  input_dim,
                  embedding_dim,
-                 decoder_input):
+                 encoder_output):
         super().__init__()
         self.embedding = Embedding(input_dim=input_dim,
                                    embedding_dim=embedding_dim)
-        self.encoder = Encoder(input_dim=input_dim)
-        self.decoder = Decoder(encoder_output=decoder_input,
-                               data_dim=input_dim)
+        self.encoder = Encoder(encoder_input=embedding_dim,
+                               encoder_output=encoder_output)
+        self.decoder = Decoder(decoder_input=encoder_output,
+                               decoder_output=input_dim)
 
     def forward(self, x):
         net = self.embedding(x)
--- source/model/decoder.py
+++ source/model/decoder.py
@@ -7,15 +7,17 @@ import torch.nn.functional as F
 
 class Decoder(nn.Module):
     def __init__(self,
-                 encoder_output,
-                 data_dim):
+                 decoder_input,
+                 decoder_output):
         super().__init__()
-        self.dense_1 = nn.Linear(in_features=encoder_output, out_features=1)
-        self.dense_1 = nn.Linear(in_features=1, out_features=12)
-        self.output = nn.Linear(in_features=12, out_features=data_dim)
+        self.dense_1 = nn.Linear(in_features=decoder_input, out_features=1)
+        self.dense_2 = nn.Linear(in_features=1, out_features=12)
+        self.dense_3 = nn.Linear(in_features=12, out_features=24)
+        self.output = nn.Linear(in_features=24, out_features=decoder_output)
 
     def forward(self, x):
         net = F.relu(self.dense_1(x))
         net = F.relu(self.dense_2(net))
+        net = F.relu(self.dense_3(net))
         net = F.relu(self.output(net))
         return net
