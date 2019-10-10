ce13e52c43aab129fcc51d50d1f127111222018c
gabriel.pontes <gabrieliprj@gmail.com>
2019-10-10 10:44:18 -0300

    get_data updated and fixed

--- source/main.py
+++ source/main.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torchtext
+
+from torchtext import data
 
 from source.model.autoencoder import AutoEncoder
 from source.model.train_model import train_model
@@ -9,7 +10,30 @@ from source.model.eval_model import evaluate
 
 
 def get_data(data_path):
-    return 1, 2, 3
+    TEXT = data.Field()
+    LABEL = data.LabelField(dtype=torch.long)
+    device = torch.device('cpu')
+
+    fields = [('input', TEXT), ('output', LABEL)]
+
+    train_data, val_data, test_data = data.TabularDataset.splits(
+        path='',
+        train='train.csv',
+        validation='val.csv',
+        test='test.csv',
+        format='csv',
+        fields=fields,
+        skip_header=True
+    )
+
+    train_iterator, val_iterator, test_iterator = data.BucketIterator.splits(
+        (train_data, val_data, test_data),
+        batch_size=256,
+        device=device,
+        sort_key=lambda x: len(x.input),
+        sort_within_batch=False)
+
+    return train_iterator, val_iterator, test_iterator
 
 
 def run_main(data_path,
@@ -23,6 +47,7 @@ def run_main(data_path,
     optimizer = optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
 
+    best_valid_loss = float('inf')
     for epoch in range(epochs):
         train_loss = train_model(model, train_iterator, optimizer, criterion)
 
