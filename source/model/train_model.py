795a6174ed8dada92847346a41cb16a7a15275a3
gabriel.pontes <gabrieliprj@gmail.com>
2019-10-10 12:15:52 -0300

    the last dimension from predictions now is removed using torch.mean

--- source/model/train_model.py
+++ source/model/train_model.py
@@ -1,20 +1,27 @@
 """
  Training the neural model.
 """
+import torch
 
 
 def train_model(model,
                 dataset_iterator,
-                criterion,
-                optimizer):
+                optimizer,
+                criterion):
+    epoch_loss = 0
+
     model.train()
     for batch in dataset_iterator:
-        predictions = model(batch.input).squeeze(1)
+        optimizer.zero_grad()
+
+        predictions = model(batch.input)
+        predictions = torch.mean(predictions, -1)
 
         loss = criterion(predictions, batch.output)
         loss.backward()
 
         optimizer.step()
-        optimizer.zero_grad()
 
+        epoch_loss += loss.item()
 
+    return epoch_loss / len(dataset_iterator)
