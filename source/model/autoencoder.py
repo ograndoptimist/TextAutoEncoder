2e2d07dab52f50aa2c0f5c0c4c895c6a41f933bc
gabriel.pontes <gabrieliprj@gmail.com>
2019-10-10 10:33:13 -0300

    params added

--- source/model/autoencoder.py
+++ source/model/autoencoder.py
@@ -9,11 +9,16 @@ from source.model.decoder import Decoder
 
 
 class AutoEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 input_dim,
+                 embedding_dim,
+                 decoder_input):
         super().__init__()
-        self.embedding = Embedding()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.embedding = Embedding(input_dim=input_dim,
+                                   embedding_dim=embedding_dim)
+        self.encoder = Encoder(input_dim=input_dim)
+        self.decoder = Decoder(encoder_output=decoder_input,
+                               data_dim=input_dim)
 
     def forward(self, x):
         net = self.embedding(x)
