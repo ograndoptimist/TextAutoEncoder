a5cdee0fa2deea49ce55cf965d1e65369bc73d8a
gabriel.pontes <gabrieliprj@gmail.com>
2019-10-21 19:36:00 -0300

    __init__ params changed to now handle models

--- source/model/autoencoder.py
+++ source/model/autoencoder.py
@@ -3,23 +3,16 @@
 """
 import torch.nn as nn
 
-from source.model.embedding import Embedding
-from source.model.encoder import Encoder
-from source.model.decoder import Decoder
-
 
 class AutoEncoder(nn.Module):
     def __init__(self,
-                 input_dim,
-                 embedding_dim,
-                 encoder_output):
+                 embedding,
+                 encoder,
+                 decoder):
         super().__init__()
-        self.embedding = Embedding(input_dim=input_dim,
-                                   embedding_dim=embedding_dim)
-        self.encoder = Encoder(encoder_input=embedding_dim,
-                               encoder_output=encoder_output)
-        self.decoder = Decoder(decoder_input=encoder_output,
-                               decoder_output=input_dim)
+        self.embedding = embedding
+        self.encoder = encoder
+        self.decoder = decoder
 
     def forward(self, x):
         net = self.embedding(x)
